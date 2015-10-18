d0f4593ec3943a465a6442613f44c8b3dc0842ea
David Anderson <dvander@alliedmods.net>
2015-10-17 19:10:43 -0700

    Remove dependence on Vendor.behavior in AMBuild scripts.

--- configure.py
+++ configure.py
@@ -12,25 +12,25 @@ except:
 		sys.stderr.write('http://www.alliedmods.net/ambuild\n')
 	sys.exit(1)
 
-run = run.PrepareBuild(sourcePath=sys.path[0])
-run.default_build_folder = 'obj-' + run.target_platform
-run.options.add_option('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
+parser = run.PrepareBuild(sourcePath=sys.path[0])
+parser.default_build_folder = 'obj-' + parser.target_platform
+parser.options.add_option('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
 		                   help='Root search folder for HL2SDKs')
-run.options.add_option('--mysql-path', type=str, dest='mysql_path', default=None,
+parser.options.add_option('--mysql-path', type=str, dest='mysql_path', default=None,
 		                   help='Path to MySQL 5')
-run.options.add_option('--mms-path', type=str, dest='mms_path', default=None,
+parser.options.add_option('--mms-path', type=str, dest='mms_path', default=None,
                        help='Path to Metamod:Source')
-run.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
+parser.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
                        help='Enable debugging symbols')
-run.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
+parser.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
                        help='Enable optimization')
-run.options.add_option('--no-mysql', action='store_false', default=True, dest='hasMySql',
+parser.options.add_option('--no-mysql', action='store_false', default=True, dest='hasMySql',
                        help='Disable building MySQL extension')
-run.options.add_option('-s', '--sdks', default='all', dest='sdks',
+parser.options.add_option('-s', '--sdks', default='all', dest='sdks',
                        help='Build against specified SDKs; valid args are "all", "present", or '
                             'comma-delimited list of engine names (default: %default)')
-run.options.add_option('--breakpad-dump', action='store_true', dest='breakpad_dump',
+parser.options.add_option('--breakpad-dump', action='store_true', dest='breakpad_dump',
 											 default=False, help='Dump and upload breakpad symbols')
-run.options.add_option('--disable-auto-versioning', action='store_true', dest='disable_auto_versioning',
+parser.options.add_option('--disable-auto-versioning', action='store_true', dest='disable_auto_versioning',
                        default=False, help='Disable the auto versioning script')
-run.Configure()
+parser.Configure()
